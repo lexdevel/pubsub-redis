b5a7c5614e516858c35b3562435e3adb1d6bc101
Alexander Logvinenko <lexdevel@gmail.com>
2021-11-06 21:10:07 +0200

    Fixed resolving error.

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,26 @@
-export * from './src/RedisPubsub.d.ts';
+import { RedisClient } from 'redis';
+
+/**
+ * Redis pub-sub wrapper class.
+ */
+export class RedisPubsub {
+  /**
+   * Constructor.
+   * @param {RedisClient} redisClient - Redis client
+   */
+  constructor(redisClient: RedisClient);
+
+  /**
+   * Publish message.
+   * @param {string} channel
+   * @param {any} message
+   */
+  publish(channel: string, message: any): void;
+
+  /**
+   * Subscribe to the channel.
+   * @param {string} channel
+   * @param {(message) => void} handler
+   */
+  subscribe(channel: string, handler: (message: any) => void): void;
+}
--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-export * from './src/RedisPubsub.js';
+import { RedisClient } from 'redis';
+
+/**
+ * Redis pub-sub wrapper class.
+ */
+export class RedisPubsub {
+  /**
+   * Constructor.
+   * @param {RedisClient} redisClient - Redis client
+   */
+  constructor(redisClient) {
+    this.redisClient = redisClient;
+    this.subscriptions = [];
+
+    this.redisClient.on('message', (channel, message) => {
+      const subscription = this.subscriptions.find(subscription => subscription.channel === channel);
+      if (subscription !== null) {
+        subscription.handler(JSON.parse(message));
+      }
+    });
+  }
+
+  /**
+   * Publish message.
+   * @param {string} channel
+   * @param {any} message
+   */
+  publish(channel, message) {
+    this.redisClient.publish(channel, JSON.stringify(message));
+  }
+
+  /**
+   * Subscribe to the channel.
+   * @param {string} channel
+   * @param {(message) => void} handler
+   */
+  subscribe(channel, handler) {
+    this.redisClient.subscribe(channel);
+    this.subscriptions.push({ channel: channel, handler: handler });
+  }
+}
